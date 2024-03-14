d878d4135c08cc11fcc1309cb6b96b3fc4637b6d
anujHopingminds <anuj@hoopinminds.com>
2024-03-14 14:30:13 +0530

    add src folder

---
+++ src/components/Poster.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Poster(){
+    return(
+        <div className="poster">
+            <img src="/Images/hamburger.jpg"/>
+            <h1>hhhhhhhh</h1>
+        </div>
+    )
+}
+
+export default Poster;
