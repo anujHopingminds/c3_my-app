b18b050e8348709cf31c9ecc8241234f2363ad93
anujHopingminds <anuj@hoopinminds.com>
2024-03-20 14:30:41 +0530

    20_march_api-intregate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,35 @@
 import React from "react";
+import { useState } from "react";
 
-function Contact(){
-    return(
+function Contact() {
+    const [name, setName] = useState('');
+    const [num, setNum] = useState('')
+
+    const handleChangeNum = (e) => {
+        setNum(e.target.value)
+    }
+
+    const handleChange = (e) => {
+        console.log(e.target.value)
+        setName(e.target.value)
+    }
+    return (
         <div className="container contact">
             <h1>Contact</h1>
             <p>We offer full-service catering for any event, large or small. We understand your needs and we will cater the food to satisfy the biggerst criteria of them all, both look and taste. Do not hesitate to contact us.</p>
             <p className="catering_test">Catering Service, 42nd Living St, 43043 New York, NY</p>
             <form>
-                <input placeholder="name" type="test" />
+                <input onChange={handleChange} placeholder="name" type="test" />
+                {name}
                 <br />
-                <input placeholder="How many people" />
+                <input onChange={handleChangeNum} placeholder="How many people" />
+                {num}
                 <br />
                 <input placeholder="Date" type="datetime-local" />
                 <br />
                 <input placeholder="Message \ Special requirements" type="text" />
-                <button >SEND MESSAGE</button>
+                <button>SEND MESSAGE</button>
+                <button>CLEAR MESSAGE</button>
             </form>
         </div>
     )
